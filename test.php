419d5a15c33ae854d4f07ad4bba466714f74a430
sjhzh <64955793+apammaaaa@users.noreply.github.com>
2020-12-28 18:07:40 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,7 @@
 <?php
   header('content-type:text/html;charset=utf-8');
   echo '欢迎' . $_POST['draft_content'];
+  $myfile = fopen('testfile.txt', 'w');
+  fwrite($myfile, 'hello world');
+  fclose($myfile);
 ?>
