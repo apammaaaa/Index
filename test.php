90fa411e9146530415d31a97f7f0b1dccb03bd96
sjhzh <64955793+apammaaaa@users.noreply.github.com>
2020-12-29 15:22:23 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,7 @@
 <?php
+  echo "<script>alert('感谢下载')</script>";
   $myfile = fopen('test.txt', 'w');
-  fwrite($myfile, $_POST['v'] . "");
+  fwrite($myfile, $_GET['v'] . "");
   fclose($myfile);
   $url = "https://apammaaaa.github.io/sprite/";
   header("Location:$url");
