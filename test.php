00a0bec54e2b1b5617ed779df6b3a0395d3a310a
sjhzh <64955793+apammaaaa@users.noreply.github.com>
2020-12-29 15:04:10 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
   $myfile = fopen('test.txt', 'w');
-  fwrite($myfile, $_GET['draft_content'] . " ");
+  fwrite($myfile, $_POST['v'] . "");
   fclose($myfile);
   $url = "https://apammaaaa.github.io/sprite/";
   header("Location:$url");
